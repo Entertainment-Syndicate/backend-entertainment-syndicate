b58f2505f9561e7b445b978e6b88706cd7c34b24
Mahmoud-Saadeh <mahmoud.saadeh998@gmail.com>
2021-06-05 17:56:50 +0300

    sent the req data

--- server.js
+++ server.js
@@ -3,26 +3,23 @@
 const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
-const cors = require('cors')
-const axios = require('axios');
+const cors = require('cors');
+// const axios = require('axios');
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-const getMovie = require('./modules/movies.js')
-const getAnime = require('./modules/anime.js')
-const getGame = require('./modules/games.js')
+const getMovie = require('./modules/movies.js');
+const getAnime = require('./modules/anime.js');
+const getGame = require('./modules/games.js');
+const fetchAllData = require('./modules/allData.js');
 
-
-
-let PORT = process.env.PORT
+let PORT = process.env.PORT;
 server.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
 
-server.get('/movies',getMovie);
+server.get('/fetchAllData', fetchAllData);
 
-server.get('/anime',getAnime)
+server.get('/anime', getAnime);
 
-server.get('/game',getGame)
-
-
+server.get('/game', getGame);
